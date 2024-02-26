ad18190b9f99359d33757de17362b9fa7064c884
ygorjg <ygorjgr@gmail.com>
2024-02-26 12:10:43 -0300

    feat: add global styles

---
+++ src/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    background-color: #FAF3F2;
+;
+}`;
+
+export default GlobalStyles;
